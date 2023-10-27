c98bb0a1c42331a7203475f2587509a9e191cc7a
Andrei-David Nan <nandavid190@gmail.com>
2023-10-27 18:43:49 +0300

    v0.10 - add unit tests for SPop operation;

--- jellyset_test.go
+++ jellyset_test.go
@@ -1,6 +1,7 @@
 package jellyset_test
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/davidandw190/jellyset"
@@ -39,4 +40,40 @@ func TestJellyset_SAdd(t *testing.T) {
 			t.Errorf("Expected to add 2 elements to myset2, but got %d", count2)
 		}
 	})
+}
+
+func TestSet_SPop(t *testing.T) {
+	set := jellyset.New()
+	set.SAdd("myset", "member1", "member2", "member3", "member4", "member5")
+
+	t.Run("Popping elements from the existing set", func(t *testing.T) {
+		popped := set.SPop("myset", 3)
+		expected := []interface{}{"member1", "member2", "member3"}
+
+		if !reflect.DeepEqual(popped, expected) {
+			t.Errorf("Expected to pop %#v, but got %#v", expected, popped)
+		}
+	})
+
+	t.Run("Popping elements from a non-existing set", func(t *testing.T) {
+		popped := set.SPop("nonexistent", 2)
+		if len(popped) != 0 {
+			t.Errorf("Expected to pop 0 elements, but got %d", len(popped))
+		}
+	})
+
+	t.Run("Popping 0 elements", func(t *testing.T) {
+		popped := set.SPop("myset", 0)
+		if len(popped) != 0 {
+			t.Errorf("Expected to pop 0 elements, but got %d", len(popped))
+		}
+	})
+
+	t.Run("Popping -1 elements", func(t *testing.T) {
+		popped := set.SPop("myset", -1)
+		if len(popped) != 0 {
+			t.Errorf("Expected to pop 0 elements, but got %d", len(popped))
+		}
+	})
+
 }
