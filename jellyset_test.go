142ef9aa6ddfa291ac1ff520ed90f82a76a7f089
Andrei-David Nan <nandavid190@gmail.com>
2023-10-28 13:44:39 +0300

    v0.16 - refactoring on unit tests structure;

--- jellyset_test.go
+++ jellyset_test.go
@@ -7,25 +7,30 @@ import (
 	"github.com/davidandw190/jellyset"
 )
 
-func TestJellyset_SAdd(t *testing.T) {
-
+func TestSet_SAdd(t *testing.T) {
 	set := jellyset.New()
 
-	t.Run("Adding elements to a new set", func(t *testing.T) {
+	t.Run("Add to New Set", func(t *testing.T) {
+		// Test adding elements to a new set.
+		// It checks if the correct number of elements was added.
 		count := set.SAdd("myset", "member1", "member2", "member3")
 		if count != 3 {
 			t.Errorf("Expected to add 3 elements, but got %d", count)
 		}
 	})
 
-	t.Run("Adding elements to an existing set", func(t *testing.T) {
+	t.Run("Add to Existing Set", func(t *testing.T) {
+		// Test adding elements to an existing set.
+		// It checks if the correct number of elements was added.
 		count := set.SAdd("myset", "member3", "member4", "member5")
 		if count != 2 {
 			t.Errorf("Expected to add 2 elements, but got %d", count)
 		}
 	})
 
-	t.Run("Adding elements to multiple sets", func(t *testing.T) {
+	t.Run("Add to Multiple Sets", func(t *testing.T) {
+		// Test adding elements to multiple sets and counting them.
+		// It ensures that the correct number of elements was added to each set.
 		set.SAdd("myset1", "member1", "member2", "member3")
 		set.SAdd("myset2", "member3", "member4", "member5")
 
@@ -45,7 +50,9 @@ func TestJellyset_SAdd(t *testing.T) {
 func TestSet_SPop(t *testing.T) {
 	set := jellyset.New()
 
-	t.Run("Popping elements from the existing set", func(t *testing.T) {
+	t.Run("Pop from Existing Set", func(t *testing.T) {
+		// Test popping elements from an existing set.
+		// It ensures that the correct number of elements is popped and returns the expected elements.
 		set.SAdd("myset", "member1", "member2", "member3", "member4", "member5")
 
 		popped := set.SPop("myset", 3)
@@ -56,7 +63,9 @@ func TestSet_SPop(t *testing.T) {
 		}
 	})
 
-	t.Run("Popping elements from a non-existing set", func(t *testing.T) {
+	t.Run("Pop from Non-Existing Set", func(t *testing.T) {
+		// Test popping elements from a non-existing set.
+		// It checks if an empty slice is returned when popping from a set that doesn't exist.
 		set.SAdd("myset", "member1", "member2", "member3", "member4", "member5")
 
 		popped := set.SPop("nonexistent", 2)
@@ -65,7 +74,9 @@ func TestSet_SPop(t *testing.T) {
 		}
 	})
 
-	t.Run("Popping 0 elements", func(t *testing.T) {
+	t.Run("Pop 0 Elements", func(t *testing.T) {
+		// Test popping 0 elements.
+		// It ensures that no elements are popped when requesting 0 elements.
 		set.SAdd("myset", "member1", "member2", "member3", "member4", "member5")
 
 		popped := set.SPop("myset", 0)
@@ -74,7 +85,9 @@ func TestSet_SPop(t *testing.T) {
 		}
 	})
 
-	t.Run("Popping -1 elements", func(t *testing.T) {
+	t.Run("Pop -1 Elements", func(t *testing.T) {
+		// Test popping -1 elements.
+		// It ensures that no elements are popped when requesting a negative number of elements.
 		set.SAdd("myset", "member1", "member2", "member3", "member4", "member5")
 
 		popped := set.SPop("myset", -1)
@@ -87,7 +100,9 @@ func TestSet_SPop(t *testing.T) {
 func TestSet_SRem(t *testing.T) {
 	set := jellyset.New()
 
-	t.Run("Removing a member from a set", func(t *testing.T) {
+	t.Run("Remove Member", func(t *testing.T) {
+		// Test removing a member from a set.
+		// It checks if the specified member is removed.
 		set.SAdd("myset", "member1", "member2", "member3")
 		removed := set.SRem("myset", "member2")
 		if !removed {
@@ -95,7 +110,9 @@ func TestSet_SRem(t *testing.T) {
 		}
 	})
 
-	t.Run("Removing a non-existent member", func(t *testing.T) {
+	t.Run("Remove Non-Existent Member", func(t *testing.T) {
+		// Test removing a non-existent member from a set.
+		// It ensures that the removal operation doesn't affect the set.
 		set.SAdd("myset", "member1", "member3")
 		removed := set.SRem("myset", "nonexistent")
 		if removed {
@@ -103,7 +120,9 @@ func TestSet_SRem(t *testing.T) {
 		}
 	})
 
-	t.Run("Removing from a non-existent set", func(t *testing.T) {
+	t.Run("Remove from Non-Existent Set", func(t *testing.T) {
+		// Test removing a member from a non-existent set.
+		// It checks that removal doesn't occur when the set doesn't exist.
 		removed := set.SRem("nonexistent", "member1")
 		if removed {
 			t.Errorf("Expected not to remove from a non-existent set, but removal occurred")
@@ -114,7 +133,9 @@ func TestSet_SRem(t *testing.T) {
 func TestSet_SRandMember(t *testing.T) {
 	set := jellyset.New()
 
-	t.Run("Retrieving from an existing set", func(t *testing.T) {
+	t.Run("Retrieve from Existing Set", func(t *testing.T) {
+		// Test retrieving random members from an existing set.
+		// It ensures the correct number of random members is retrieved.
 		set.SAdd("myset", "member1", "member2", "member3", "member4", "member5")
 		randomMembers := set.SRandMember("myset", 3)
 		if len(randomMembers) != 3 {
@@ -122,7 +143,9 @@ func TestSet_SRandMember(t *testing.T) {
 		}
 	})
 
-	t.Run("Retrieving from a non-existing set", func(t *testing.T) {
+	t.Run("Retrieve from Non-Existent Set", func(t *testing.T) {
+		// Test retrieving random members from a non-existent set.
+		// It ensures that an empty slice is returned when retrieving from a set that doesn't exist.
 		set.SAdd("myset", "member1", "member2", "member3", "member4", "member5")
 		randomMembers := set.SRandMember("nonexistent", 3)
 		if len(randomMembers) != 0 {
@@ -130,7 +153,9 @@ func TestSet_SRandMember(t *testing.T) {
 		}
 	})
 
-	t.Run("Retrieving 0 random members", func(t *testing.T) {
+	t.Run("Retrieve 0 Random Members", func(t *testing.T) {
+		// Test retrieving 0 random members.
+		// It ensures that no elements are retrieved when requesting 0 random members.
 		set.SAdd("myset", "member1", "member2", "member3", "member4", "member5")
 		randomMembers := set.SRandMember("myset", 0)
 		if len(randomMembers) != 0 {
@@ -139,41 +164,12 @@ func TestSet_SRandMember(t *testing.T) {
 	})
 }
 
-func TestSet_SIsMember(t *testing.T) {
-	set := jellyset.New()
-
-	t.Run("Checking for an existing member in a set", func(t *testing.T) {
-		set.SAdd("myset", "member1", "member2", "member3")
-		exists := set.SIsMember("myset", "member2")
-
-		if !exists {
-			t.Errorf("Expected member2 to exist in the set, but it doesn't")
-		}
-	})
-
-	t.Run("Checking for a non-existing member in a set", func(t *testing.T) {
-		set.SAdd("myset", "member1", "member2", "member3")
-		exists := set.SIsMember("myset", "nonexistent")
-
-		if exists {
-			t.Errorf("Expected nonexistent to not exist in the set, but it does")
-		}
-	})
-
-	t.Run("Checking for a member in a non-existing set", func(t *testing.T) {
-		set.SAdd("myset", "member1", "member2", "member3")
-		exists := set.SIsMember("nonexistent", "member1")
-
-		if exists {
-			t.Errorf("Expected member1 to not exist in the nonexistent set, but it does")
-		}
-	})
-}
-
 func Test_SMove(t *testing.T) {
 	set := jellyset.New()
 
-	t.Run("Moving a member from a source set to a destination set", func(t *testing.T) {
+	t.Run("Move Member", func(t *testing.T) {
+		// Test moving a member from a source set to a destination set.
+		// It checks if the member is successfully moved from the source set to the destination set.
 		set.SAdd("sourceSet", "member1", "member2")
 		moved := set.SMove("sourceSet", "destSet", "member2")
 
@@ -182,7 +178,9 @@ func Test_SMove(t *testing.T) {
 		}
 	})
 
-	t.Run("Moving a non-existing member from a source set to a destination set", func(t *testing.T) {
+	t.Run("Move Non-Existent Member", func(t *testing.T) {
+		// Test moving a non-existent member from a source set to a destination set.
+		// It ensures that no movement occurs when the member doesn't exist.
 		set.SAdd("sourceSet", "member1", "member2")
 		moved := set.SMove("sourceSet", "destSet", "nonexistent")
 
@@ -191,8 +189,9 @@ func Test_SMove(t *testing.T) {
 		}
 	})
 
-	t.Run("Moving a member from a non-existing source set to a destination set", func(t *testing.T) {
-		set.SAdd("sourceSet", "member1", "member2")
+	t.Run("Move from Non-Existent Source Set", func(t *testing.T) {
+		// Test moving a member from a non-existent source set to a destination set.
+		// It ensures that no movement occurs when the source set doesn't exist.
 		moved := set.SMove("nonexistentSource", "destSet", "member1")
 
 		if moved {
@@ -200,7 +199,9 @@ func Test_SMove(t *testing.T) {
 		}
 	})
 
-	t.Run("Moving a member from a source set to a non-existing destination set", func(t *testing.T) {
+	t.Run("Move to Non-Existent Dest Set", func(t *testing.T) {
+		// Test moving a member from a source set to a non-existent destination set.
+		// It checks that the destination set is created, and the member is moved.
 		set.SAdd("sourceSet", "member1", "member2")
 		moved := set.SMove("sourceSet", "nonexistentDest", "member1")
 
